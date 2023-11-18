c4bb9fe1067b7f17a67257c15e8ca177ceb9ce30
habidoyetech <abidoyeabiodun97@gmail.com>
2023-11-18 14:40:18 +0100

    create a database schema and connected to my mongoose

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,14 +12,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
         <body>
-            <div className='name'>
-                <div className='gradient'/>
-            </div>
+            <Provider>
+                <div className='name'>
+                    <div className='gradient'/>
+                </div>
 
-            <main className='app'>
-                <Nav/>
-                {children}
-            </main>
+                <main className='app'>
+                    <Nav/>
+                    {children}
+                </main>
+            </Provider>
         </body>
 
     </html>
